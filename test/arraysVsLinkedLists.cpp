55318831c3fb1512a187aca93bdf7999cecf3bd0
Yassermsdkk <yassermoussaddak21@gmail.com>
2023-06-18 11:22:19 +0300

    Create arraysVsLinkedLists.cpp

---
+++ test/arraysVsLinkedLists.cpp
@@ -0,0 +1,22 @@
+/*
+
+    arrays can find its elements using an index
+    arrays have a fixed size
+    when you allocate memory for an arrays it stays the same forever which means you can add or take anything from it   
+    since elements are ordered by indexes you know the location of each elements since you know it's index
+
+*/
+
+
+
+/*
+    linked lists have two values the value of the element and a pointer that pointer that points to next untill the final one
+    linked lists have dynamic size
+    you can add and remove elements freely
+    1 points to 2 points to 3 points to 4 points to 5
+    since the linked elements are linked this way you can't access forexample element 4 directly
+    you have to go all the way from 1 since it's the only element you know the location
+
+    need more memory since you store two values the pointer as well as element value
+
+*/
