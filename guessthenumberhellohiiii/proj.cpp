5ec437244fd94ca02aa4dfce952c04994f30f674
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-01-27 17:10:23 +0200

    new program to rego over everythig

---
+++ guessthenumberhellohiiii/proj.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = rand() % 100;
+    int guess;
+    cout << "pick a number from 1 to 100";
+    cin >> guess;
+    
+}
+// The computer selects a random number between a specified range
+// (e.g., 1 to 100).
+
+// The player has a limited number of attempts to guess the number.
+
+// After each guess, the game provides feedback to the player whether
+// their guess was too high, too low, or correct.
